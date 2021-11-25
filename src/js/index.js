d5a15eaa3d8ad248f1512bac85243d5b24910501
Mario Jose Rodriguez Machacon <75395682+mrodriguezm21@users.noreply.github.com>
2021-11-25 14:58:03 -0500

    feat: media querie_ amount of products

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,11 @@
 console.log("Dev m3");
+// Filter duplicated products
+let noRepeat = [];
+function nonRepeat(id) {
+  const val = noRepeat.includes(id);
+  if (!val) noRepeat.push(id);
+  return val;
+}
 
 /* Filter menu mobile */
 
@@ -12,30 +19,28 @@ let colorUl = document.getElementsByClassName("color-ul")[0];
 let colorUl2 = document.getElementsByClassName("expand-colors-ul")[0];
 let sizeMenu = document.getElementsByClassName("sizes")[0];
 let sizeUl = document.getElementsByClassName("size-ul")[0];
-let priceMenu =document.getElementsByClassName("price-range")[0];
+let priceMenu = document.getElementsByClassName("price-range")[0];
 let priceUl = document.getElementsByClassName("p-container")[0];
 let h1Filter = document.getElementById("filt-header");
 let xButton = document.getElementById("x");
 let bagIcon = document.getElementById("bag-icon");
 let logo = document.getElementById("logo");
 
-
 filterButton.addEventListener("click", () => {
-    categoryTitle.style.display = "none";
-    logo.style.display = "none";
-    bagIcon.style.display = "none";
-    filterMenu.classList.toggle("is-active");
-    h1Filter.classList.toggle("is-active");
-    xButton.classList.toggle("is-active");
-
+  categoryTitle.style.display = "none";
+  logo.style.display = "none";
+  bagIcon.style.display = "none";
+  filterMenu.classList.toggle("is-active");
+  h1Filter.classList.toggle("is-active");
+  xButton.classList.toggle("is-active");
 });
 xButton.addEventListener("click", () => {
-    categoryTitle.style.display = "unset";
-    logo.style.display = "unset";
-    bagIcon.style.display = "unset";
-    filterMenu.classList.toggle("is-active");
-    h1Filter.classList.toggle("is-active");
-    xButton.classList.toggle("is-active");
+  categoryTitle.style.display = "unset";
+  logo.style.display = "unset";
+  bagIcon.style.display = "unset";
+  filterMenu.classList.toggle("is-active");
+  h1Filter.classList.toggle("is-active");
+  xButton.classList.toggle("is-active");
 });
 colorMenu.addEventListener("click", () => {
   colorUl.classList.toggle("is-active");
@@ -43,21 +48,13 @@ colorMenu.addEventListener("click", () => {
 });
 
 sizeMenu.addEventListener("click", () => {
-    sizeUl.classList.toggle("is-activeG");
-})
+  sizeUl.classList.toggle("is-activeG");
+});
 priceMenu.addEventListener("click", () => {
-    priceUl.classList.toggle("is-active");
-})
+  priceUl.classList.toggle("is-active");
+});
 
 /* Sort menu mobile */
-
-
-
-
-
-
-
-
 
 /* Get the elements from API */
 
@@ -65,53 +62,28 @@ priceMenu.addEventListener("click", () => {
 let productContainer = document.getElementById("items-container");
 let seeMoreButton = document.getElementById("see-more");
 
-function getSixProducts() {
+// Get products
+function getProducts(amount) {
   fetch("http://localhost:5000/products")
     .then((response) => {
       return response.json();
     })
     .then((data) => {
-      const slicedArray = data.slice(0, 6);
-      console.log(slicedArray);
+      let slicedArray = data.slice(0, amount);
       slicedArray.forEach((element) => {
         let { id, name, price, image, parcelamento } = element;
-        productContainer.innerHTML += `
-                <div class="producto" id="producto-${id}">
-                <div class="product-image">
-                  <img src=${image} alt="" />
-                </div>
-                <span class="product-name">${name}</span>
-                <span class="product-price">R$ ${price}</span>
-                <span class="product-quotas">até ${parcelamento[0]}x de R$${parcelamento[1]}</span>
-                <button class="button product-button">COMPRAR</button>
-              </div>`;
-      });
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-}
-// Get more products
-function getProducts() {
-  fetch("http://localhost:5000/products")
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      const slicedArray = data.slice(0, 9);
-
-      slicedArray.forEach((element) => {
-        let { id, name, price, image, parcelamento } = element;
-        productContainer.innerHTML += `
-                <div class="producto" id="producto-${id}">
-                <div class="product-image">
-                  <img src=${image} alt="" />
-                </div>
-                <span class="product-name">${name}</span>
-                <span class="product-price">R$ ${price}</span>
-                <span class="product-quotas">até ${parcelamento[0]}x de R$${parcelamento[1]}</span>
-                <button class="button product-button">COMPRAR</button>
-              </div>`;
+        if (!nonRepeat(id)) {
+          productContainer.innerHTML += `
+          <div class="producto" id="producto-${id}">
+          <div class="product-image">
+          <img src=${image} alt="" />
+          </div>
+          <span class="product-name">${name}</span>
+          <span class="product-price">R$ ${price}</span>
+          <span class="product-quotas">até ${parcelamento[0]}x de R$${parcelamento[1]}</span>
+          <button class="button product-button">COMPRAR</button>
+          </div>`;
+        }
       });
     })
     .catch((error) => {
@@ -119,11 +91,21 @@ function getProducts() {
     });
 }
 
+// Get products media querie
+const product = () => {
+  if (window.matchMedia("(max-width: 768px)").matches) {
+    getProducts(4);
+    return null;
+  } else {
+    getProducts();
+  }
+};
+// Get more products (for mobile)
 seeMoreButton.addEventListener("click", () => {
   productContainer.innerHTML = "";
-  getProducts();
+  noRepeat = [];
+  getProducts(9);
 });
 
 // Run the function
-// getSixProducts();
-getProducts();
+product();
